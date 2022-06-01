2271a96c2843930c81c6020d3cc3c0c39feaae67
KatieGuli <gulikat7528@gmail.com>
2022-05-31 21:52:45 -0400

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -5,9 +5,9 @@ public class gitReview01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.print("today we learn github");
-	System.out.print("today is a very good dAY");
-		
+	System.out.println("today we learn github");
+	System.out.println("today is a very good dAY");
+	System.out.println("edit");
 }
 }
 
