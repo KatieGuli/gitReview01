6e4d78b975c9c164abdedbae4e9370bbca3f7cff
KatieGuli <gulikat7528@gmail.com>
2022-05-31 21:35:44 -0400

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,14 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.print("today we learn github");
+	System.out.print("today is a very good dAY");
+		
+}
+}
+
+
